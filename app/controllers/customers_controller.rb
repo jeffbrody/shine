54a058c1a5caf69c1b35da7afe49d3321d131c42
Jeff Brody <jbrodyaiki@gmail.com>
2018-06-21 12:02:10 -0700

    End ch 5 add Bootstrap

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
+    PAGE_SIZE = 10
     def index
+        @page = (params[:page] || 0).to_i
         if params.has_key? :keywords
             @keywords = params[:keywords]
             customer_search_term = CustomerSearchTerm.new(@keywords)
@@ -8,7 +10,8 @@ class CustomersController < ApplicationController
              @customers = Customer.where(
                  customer_search_term.where_clause,
                  customer_search_term.where_args).
-                 order(customer_search_term.order).limit(10)
+                 order(customer_search_term.order).
+                 offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
          else
              @customers = Customer.all.limit(10)
          end
